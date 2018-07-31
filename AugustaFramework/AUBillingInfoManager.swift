a42ce9a9c36e0d4df75ba5067e50d9e43637e6d7
augusta-Ranjith <>
2018-07-31 17:27:39 +0530

    added camera scanning for cards

--- AugustaFramework/AUBillingInfoManager.swift
+++ AugustaFramework/AUBillingInfoManager.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import UIKit
+import PayCardsRecognizer
+
+public protocol AUBillingCardScanningDelegate{
+    func cameraDidScanCard(with Result: PayCardsRecognizerResult)
+}
+
+public class AUBillingInfoManager{
+    
+    var recognizer: PayCardsRecognizer?
+    var delegate: AUBillingCardScanningDelegate?
+    public func startScan(from view: UIView, borderColor: UIColor, mode: PayCardsRecognizerDataMode = .number, resultMode: PayCardsRecognizerResultMode = .sync){
+        recognizer = PayCardsRecognizer(delegate: self, recognizerMode: .number, resultMode: .sync, container: view, frameColor: borderColor)
+        recognizer?.startCamera()
+    }
+    
+    public func stopScan(){
+        if(recognizer != nil){
+            recognizer?.stopCamera()
+        }
+    }
+    
+}
+
+extension AUBillingInfoManager: PayCardsRecognizerPlatformDelegate{
+    
+    // PayCardsRecognizerPlatformDelegate
+    
+    public func payCardsRecognizer(_ payCardsRecognizer: PayCardsRecognizer, didRecognize result: PayCardsRecognizerResult) {
+        recognizer?.stopCamera()
+        self.delegate?.cameraDidScanCard(with: result)
+//        cardNoTextField.text = result.recognizedNumber // Card number
+//        cardNameTextField.text = result.recognizedHolderName // Card holder
+//        cardMonthTextField.text = result.recognizedExpireDateMonth // Expire month
+//        cardYearTextField.text = result.recognizedExpireDateYear // Expire year
+//        (scanButton as! UIButton).setTitle("Scan Card", for: UIControlState.normal)
+    }
+}
